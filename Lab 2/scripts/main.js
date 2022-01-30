b736ba2c427ab5148ea5752e7975b6aef48d52df
robynlafleche <71790839+robynlafleche@users.noreply.github.com>
2022-01-29 22:07:29 -0500

    Added changes regarding the products

--- Lab 2/scripts/main.js
+++ Lab 2/scripts/main.js
@@ -12,3 +12,33 @@ function getPage(elem,hide1,hide2) {
 
     page.style.display = value;
 }
+
+/*code inspired from youtube tutorial https://www.google.com/search?q=add+to+cart+button+html+and+javascript&oq=&aqs=chrome.0.35i39i362l8.147681j0j7&sourceid=chrome&ie=UTF-8#kpvalbx=_Mrz1Yc3LF5-aptQP1NC7uA820
+products is an array containing the names of the products added to the cart
+cartTotal is the total after all the items are added to the cart*/
+
+var products = []
+var cartTotal = 0
+
+/*Adds the items selected by the user to the cart*/
+function cartedItems(name) { 
+  products.push(name)
+  var priceName = name + "Price"
+  var quantityName = name + "Quantity"
+  var price = parseFloat(document.getElementById(priceName).innerHTML.replace('$', '')) * document.getElementById(quantityName).value
+  updateCartTotal(price);
+}
+
+/*Updates the cart total with the prices of each item multipled by their quantities*/
+function updateCartTotal(price) {
+  cartTotal += price
+  alert(cartTotal)
+}
+
+/*Displays only the items selected by the user*/
+function displayProducts(value) {
+    var className = document.getElementsByClassName(value)
+    for (var i = 0; i < className.length; i++) {
+        className[i].style.display = "none"
+    }
+    }
