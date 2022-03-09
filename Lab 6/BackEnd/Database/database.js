ae7746266a9380e8067157c5e647a823e0bb7cea
KDT2021 <kdahe094@hotmail.com>
2022-03-09 07:15:18 -0500

    All 3 DB tables have been created.

--- Lab 6/BackEnd/Database/database.js
+++ Lab 6/BackEnd/Database/database.js
@@ -1,4 +1,6 @@
 
+// mySql for Node JS, how to connect the database obtained from https://www.w3schools.com/nodejs/nodejs_mysql.asp
+
 var mysql = require('mysql');
 
 // create the connection to the database with desired credentials
@@ -7,7 +9,6 @@ var conn = mysql.createConnection({
 	port: 3306,
 	user: "root",
 	password: "group6",
-	insecureAuth : true,
 });
 
 
@@ -20,11 +21,32 @@ conn.connect(function(err) {
 			throw err;
 		console.log("Database successfully created!");
 	});
-	var sql = "CREATE TABLE IF NOT EXISTS group6db.users (userID int NOT NULL AUTO_INCREMENT, firstName VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), phoneNumber VARCHAR(255), PRIMARY KEY (userID))";
+
+	var sql = "CREATE TABLE IF NOT EXISTS group6db.users (userID int AUTO_INCREMENT, firstName VARCHAR(255), lastName VARCHAR(255), email VARCHAR(255), phoneNumber VARCHAR(255), PRIMARY KEY (userID))";
   	conn.query(sql, function (err, result) {
     if (err) 
     	throw err;
-    console.log("Table created");
-  });
+    console.log("users table created");
+
+  	});
+
+  	sql = "CREATE TABLE IF NOT EXISTS group6db.surveyResults (surveyResultID int NOT NULL AUTO_INCREMENT, \
+	budgetRange VARCHAR(255), highlightLocation VARCHAR(255), readability VARCHAR(255), \
+	recommendation VARCHAR(255), starRating VARCHAR(255), easinessLevel VARCHAR(255), difficultyLevel VARCHAR(255), suggestion VARCHAR(255), \
+	userID INTEGER, PRIMARY KEY (surveyResultID))";
+	conn.query(sql, function (err, result) {
+	if (err) 
+		throw err;
+	console.log("surveyResults table created");	
+	});
+
+
+	sql = "CREATE TABLE IF NOT EXISTS group6db.favouriteFeatures (favouriteFeatureResultID int NOT NULL AUTO_INCREMENT, \
+		favouriteFeatureResultChoice VARCHAR(255), surveyResultID INTEGER, PRIMARY KEY (favouriteFeatureResultID))";
+	conn.query(sql, function (err, result) {
+	if (err) 
+		throw err;
+	console.log("favouriteFeatures table created");		
+	});
 
 });
