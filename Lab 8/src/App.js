4f46d14617ee036950dd56d048ed0e134b5a7f82
Meaghan Clarkin <mclar063@uottawa.ca>
2022-03-21 23:27:25 -0400

    changes to file structure

--- Lab 8/src/App.js
+++ Lab 8/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/home';
+import Booking from './pages/booking';
+import FAQ from './pages/faq';
+import Rooms from './pages/rooms';
+import TeamBuilding from './pages/team-building';
+import Contact from './pages/contact';
+import Admin from './pages/admin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar fill variant="tabs" defaultActiveKey="/home"/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/booking' element={<Booking/>} />
+        <Route path='/faq' element={<FAQ/>} />
+        <Route path='/rooms' element={<Rooms/>} />
+        <Route path='/team-building' element={<TeamBuilding/>} />
+        <Route path='/admin' element={<Admin/>} />
+      </Routes>
+    </Router>
   );
 }
 
