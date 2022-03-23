0d373ff7b1feee1a1118703c8840227264206646
Meaghan Clarkin <mclar063@uottawa.ca>
2022-03-23 02:34:25 -0400

    changes to homepage

--- Lab 8/src/pages/home.js
+++ Lab 8/src/pages/home.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import './home.css';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
+	let navigate = useNavigate();
+	const bookNowButton = () => {
+		let path = '/booking'
+		navigate(path);
+	}
 return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>Divide, conquer, escape!</h1>
+	<div class="background-img">
+	<h1>DIVIDE </h1>
+	<h1>CONQUER </h1>
+	<h1>ESCAPE</h1>
+	<h2> Book an escape room today </h2>
+	<Button onClick={bookNowButton}>Book Now</Button>;
 	</div>
 	);
 };
