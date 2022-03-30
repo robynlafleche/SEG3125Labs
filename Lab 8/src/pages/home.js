7f8a12334e86c80e4c28a2c312e5b53614f1541c
KDT2021 <79962022+KDT2021@users.noreply.github.com>
2022-03-30 09:46:50 -0400

    DB modules and internationalization start. (#30)

--- Lab 8/src/pages/home.js
+++ Lab 8/src/pages/home.js
@@ -4,22 +4,33 @@ import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Button } from 'react-bootstrap';
 
+import { useTranslation} from 'react-i18next';
+
+const lngs = {
+	en: { nativeName: 'English' },
+	fr: { nativeName: 'Francais' }
+  };
+
 const Home = () => {
 	let navigate = useNavigate();
 	const bookNowButton = () => {
 		let path = '/booking'
 		navigate(path);
 	}
+
+	const { t, i18n } = useTranslation();
+
+
 return (
 
 	<div className = 'backgroundImg'>
 	<div className = 'home'>
-	<h1>DIVIDE </h1>
-	<h1>CONQUER </h1>
-	<h1>ESCAPE</h1>
+	<h1>{t('description.devisePart1')}</h1>
+	<h1>{t('description.devisePart2')}</h1>
+	<h1>{t('description.devisePart3')}</h1>
 	</div>
-	<h2> Book an escape room today </h2>
-	<Button variant="outline" onClick={bookNowButton}>Book Now</Button>
+	<h2> {t('description.bookEscapeRoomText')} </h2>
+	<Button variant="outline" onClick={bookNowButton}>{t('description.bookNow')}</Button>
 	</div>
 	);
 }
