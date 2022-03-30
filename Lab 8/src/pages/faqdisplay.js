5fc73a0c9a585dd6416ed60d2b825ae822d13b0a
robynlafleche <71790839+robynlafleche@users.noreply.github.com>
2022-03-30 12:40:05 -0400

    translationg for FAQ Page

--- Lab 8/src/pages/faqdisplay.js
+++ Lab 8/src/pages/faqdisplay.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
+import { useTranslation} from 'react-i18next';
+
 function Faqdisplay ({faq, index}){
+
+    const { t, i18n } = useTranslation();
+
     return (
         <div 
             className={"faq" + (faq.open ? 'open' : '')}
             key={index}
         >
             <div className="faq-question">
-                <h3>{faq.question}</h3>
+                <h3>{t('description.question' + index)}</h3>
             </div>
             <div className="faq-answer">
-                <h5>{faq.answer}</h5>
+                <h5>{t('description.answer' + index)}</h5>
             </div>
         </div>
     )
